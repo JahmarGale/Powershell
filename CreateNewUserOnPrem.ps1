19880b2d614ba46c2ccfdb41fb4342f9d70ff02d
Jahmar Gale <71678091+JahmarGale@users.noreply.github.com>
2025-07-09 13:35:47 -0400

    Add files via upload

---
+++ CreateNewUserOnPrem.ps1
@@ -0,0 +1,7 @@
+$Name = 'John Doe'
+$GivenName = 'John'
+$Surname = 'Doe'
+$SamAccountName = 'jdoe'
+$UserPrincipalName = 'jdoe@yourdomain.com'
+$AccountPassword = 'Jdoe54321'
+New-ADUser -Name $Name -GivenName $GivenName  -Surname $Surname -SamAccountName $SamAccountName -UserPrincipalName $UserPrincipalName -AccountPassword (ConvertTo-SecureString $AccountPassword -AsPlainText -Force) -Enabled $true -Path "OU=Users,OU=Site,OU=City,OU=Offices,DC=YourDC,DC=org"
